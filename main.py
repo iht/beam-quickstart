45c639f648d69aef628da085ed93dfe1335f08bb
Israel Herraiz <ihr@google.com>
2021-03-24 20:00:37 +0100

    WIP

--- main.py
+++ main.py
@@ -11,3 +11,40 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+from typing import Tuple
+
+import apache_beam as beam
+import argparse
+
+from apache_beam import PCollection
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Nuestro primer pipeline")
+  parser.add_argument("--entrada", help="Fichero de entrada")
+  parser.add_argument("--salida", help="Fichero de salida")
+
+  our_args, beam_args = parser.parse_known_args()
+  run_pipeline(our_args, beam_args)
+
+
+def run_pipeline(custom_args, beam_args):
+  entrada = custom_args.entrada
+  salida = custom_args.salida
+
+  opts = PipelineOptions(beam_args)
+
+  with beam.Pipeline(opts) as p:
+    lineas: PCollection[str] = p | beam.io.ReadFromText(entrada)
+    # "En un lugar de La Mancha" --> ["En", "un", ...], [...], [...] --> "En", "un", "lugar", ....
+    palabras = lineas | beam.FlatMap(lambda l: l.split())
+    contadas: PCollection[Tuple[str, int]] = palabras | beam.combiners.Count.PerElement()
+    # "En" -> ("En", 17)
+    # "un" -> ("un", 28)
+    palabras_top = contadas | beam.combiners.Top.Of(5, keys=lambda kv: kv[1])
+    palabras_top | beam.Map(print)
+
+
+if __name__ == '__main__':
+  main()
