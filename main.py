10ff49dc0cb24e6d6d6ef80c0c96d608d7ae0774
Israel Herraiz <ihr@google.com>
2021-03-24 20:23:29 +0100

    Casi listo

--- main.py
+++ main.py
@@ -20,10 +20,28 @@ from apache_beam import PCollection
 from apache_beam.options.pipeline_options import PipelineOptions
 
 
+def sanitizar_palabra(palabra):
+  para_quitar = [',', '.', '-', ':', ' ', "'", '"']
+
+  for simbolo in para_quitar:
+    palabra = palabra.replace(simbolo, '')
+
+  palabra = palabra.lower()
+
+  palabra = palabra.replace("á", "a")
+  palabra = palabra.replace("é", "e")
+  palabra = palabra.replace("í", "i")
+  palabra = palabra.replace("ó", "o")
+  palabra = palabra.replace("ú", "u")
+
+  return palabra
+
+
 def main():
   parser = argparse.ArgumentParser(description="Nuestro primer pipeline")
   parser.add_argument("--entrada", help="Fichero de entrada")
   parser.add_argument("--salida", help="Fichero de salida")
+  parser.add_argument("--n-palabras", type=int, help="Número de palabras en la salida")
 
   our_args, beam_args = parser.parse_known_args()
   run_pipeline(our_args, beam_args)
@@ -32,6 +50,7 @@ def main():
 def run_pipeline(custom_args, beam_args):
   entrada = custom_args.entrada
   salida = custom_args.salida
+  n_palabras = custom_args.n_palabras
 
   opts = PipelineOptions(beam_args)
 
@@ -39,10 +58,11 @@ def run_pipeline(custom_args, beam_args):
     lineas: PCollection[str] = p | beam.io.ReadFromText(entrada)
     # "En un lugar de La Mancha" --> ["En", "un", ...], [...], [...] --> "En", "un", "lugar", ....
     palabras = lineas | beam.FlatMap(lambda l: l.split())
-    contadas: PCollection[Tuple[str, int]] = palabras | beam.combiners.Count.PerElement()
+    limpiadas = palabras | beam.Map(sanitizar_palabra)
+    contadas: PCollection[Tuple[str, int]] = limpiadas | beam.combiners.Count.PerElement()
     # "En" -> ("En", 17)
     # "un" -> ("un", 28)
-    palabras_top_lista = contadas | beam.combiners.Top.Of(5, key=lambda kv: kv[1])
+    palabras_top_lista = contadas | beam.combiners.Top.Of(n_palabras, key=lambda kv: kv[1])
     palabras_top = palabras_top_lista | beam.FlatMap(lambda x: x)
     formateado: PCollection[str] = palabras_top | beam.Map(lambda kv: "%s,%d" % (kv[0], kv[1]))
     formateado | beam.io.WriteToText(salida)
