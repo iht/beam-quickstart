5a70b33d456dedf442e452f200ff8e2c6ffb7f73
Israel Herraiz <ihr@google.com>
2022-03-03 10:59:15 +0100

    Primer borrador

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+from typing import List
 
 import apache_beam as beam
 import argparse
@@ -20,7 +21,14 @@ from apache_beam.options.pipeline_options import PipelineOptions
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--fichero-entrada", required=True)
+    parser.add_argument("--fichero-salida", required=True)
+    parser.add_argument("--num-palabras", required=True)
+
+    known_args, dataflow_args = parser.parse_known_args()
+    run_pipeline(known_args, dataflow_args)
 
 
 def sanitizar_palabra(palabra: str) -> str:
@@ -40,11 +48,20 @@ def sanitizar_palabra(palabra: str) -> str:
     return palabra
 
 
+def lista_a_csv(l: List[(str, int)]) -> str:
+    csv = ""
+    for e in l:
+        csv += f"{e[0]},{e[1]}\n"
+
+    return csv
+
+
 def run_pipeline(my_args, dataflow_args):
     opts = PipelineOptions(dataflow_args)
 
-    input_file = my_args.input_file
-    output_file = my_args.output_file
+    input_file = my_args.fichero_entrada
+    output_file = my_args.fichero_salida
+    num_palabras = my_args.num_palabras
 
     with beam.pipeline.Pipeline(options=opts) as p:
         # entrada | "etiqueta" >> transform()
@@ -53,3 +70,17 @@ def run_pipeline(my_args, dataflow_args):
         # ["En", "un", "lugar", ....]
         palabras: PCollection[str] = entrada | "Partir en palabras" >> \
                                      beam.FlatMap(lambda l: l.split())
+
+        # ["en", "un", "lugar", ....]
+        palabras_limpias = palabras | "Limpiar palabras" >> beam.Map(sanitizar_palabra)
+
+        # ("en", 7), ("sancho", 20), ("dulcinea", ???), ("mancha", 14)
+        contadas = palabras_limpias | "Contar" >> beam.combiners.Count.PerElement()
+
+        top_palabras = contadas | "Top" >> beam.combiners.Top.Of(num_palabras)
+        lineas_csv: PCollection[str] = top_palabras | "A CSV" >> beam.Map(lista_a_csv)
+        lineas_csv | "Escribir salida" >> beam.io.WriteToText(output_file)
+
+
+if __name__ == '__main__':
+    main()
