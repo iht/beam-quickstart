6442b679aeb3ff819e3d7dca1a5d0b4a31e677ed
Israel Herraiz <ihr@google.com>
2022-03-03 10:40:56 +0100

    WIP

--- main.py
+++ main.py
@@ -11,3 +11,45 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+
+import apache_beam as beam
+import argparse
+
+from apache_beam import PCollection
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+def main():
+    pass
+
+
+def sanitizar_palabra(palabra: str) -> str:
+    quitar = [",", ".", "?", "!", "-", "_"]
+    for c in quitar:
+        if c in palabra:
+            palabra = palabra.replace(c, '')
+
+    palabra = palabra.lower()
+
+    palabra = palabra.replace("á", "a")
+    palabra = palabra.replace("é", "e")
+    palabra = palabra.replace("í", "i")
+    palabra = palabra.replace("ó", "o")
+    palabra = palabra.replace("ú", "u")
+
+    return palabra
+
+
+def run_pipeline(my_args, dataflow_args):
+    opts = PipelineOptions(dataflow_args)
+
+    input_file = my_args.input_file
+    output_file = my_args.output_file
+
+    with beam.pipeline.Pipeline(options=opts) as p:
+        # entrada | "etiqueta" >> transform()
+        entrada: PCollection[str] = p | "Leer datos" >> beam.io.ReadFromText(input_file)
+        # "En un lugar de La Mancha", "lo que sea"
+        # ["En", "un", "lugar", ....]
+        palabras: PCollection[str] = entrada | "Partir en palabras" >> \
+                                     beam.FlatMap(lambda l: l.split())
